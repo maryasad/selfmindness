d94981b7d70e0666d04e5ad004f4309a175edca5
maryasad <maryasad76@yahoo.com>
2024-12-27 16:24:54 +0100

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/selfmindness/',
   plugins: [react()],
   server: {
     port: 3002, // Using port 3002 since 3000 and 3001 are taken
